0c14880ba2bbf3bc88d85ac3696bb2f88688435e
min <mjk12300@naver.com>
2025-11-21 17:28:54 +0900

    c++ 환경설정, 테스트

---
+++ 1.c
@@ -0,0 +1,11 @@
+//gcc 1.c -o 1 이란 명령은 gcc는 C언어이고, 1.c 을 컴파일해서 1.exe 실행 파일로 만들어라.
+//./1  이란 명령은 1.exe 실행 파일을 실행하라.
+
+#include <stdio.h>
+int main(void) {
+
+    printf("congrats!");
+
+    return 0;
+    
+}
---
+++ 2.cpp
@@ -0,0 +1,10 @@
+//g++ 2.cpp -o 2 이란 명령은 g++는 C++언어이고, 2.cpp 을 컴파일해서 2.exe 실행 파일로 만들어라.
+//./2  이란 명령은 2.exe 실행 파일을 실행하라.
+
+#include <iostream>
+
+int main(void) {
+
+    std::cout << "nice ecoding!!";
+    return 0;   
+}
